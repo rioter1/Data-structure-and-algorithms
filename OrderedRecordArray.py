2223004d10d4979f4e1c84b174f91e6ac2874688
rioter1 <prateek.gsharma@gmail.com>
2023-07-04 23:23:02 +0530

    adding ordered record array

---
+++ OrderedRecordArray.py
@@ -0,0 +1,39 @@
+# Implement an Ordered Array of Records structure
+def identity(x):
+    return x
+# The identity function
+class OrderedRecordArray(object):
+    def __init__(self, initialSize, key=identity):
+        # Constructor
+        self.__a = [None] * initialSize # The array stored as a list
+        self.__nItems = 0 # No items in array initially
+        self.__key = key
+    # Key function gets record key
+    def __len__(self):
+        # Special def for len() func
+        return self.__nItems
+    # Return number of items
+    def get(self, n):
+        # Return the value at index n
+        if n >= 0 and n < self.__nItems: # Check if n is in bounds, and
+            return self.__a[n]
+        # only return item if in bounds
+        raise IndexError("Index " + str(n) + " is out of range")
+    def traverse(self, function=print): # Traverse all items
+        for j in range(self.__nItems):
+            # and apply a function
+            function(self.__a[j])
+    def __str__(self):
+        # Special def for str() func
+        ans = "["
+        # Surround with square brackets
+        for i in range(self.__nItems):
+        # Loop through items
+            if len(ans) > 1:
+                # Except next to left bracket,
+                ans += ", " # separate items with comma
+                ans += str(self.__a[i])
+                # Add string form of item
+                ans += "]"
+            # Close with right bracket
+        return ans
